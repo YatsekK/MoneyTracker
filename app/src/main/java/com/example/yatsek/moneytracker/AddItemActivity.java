9d85626b721fce2b50c608aaa2e386085225be7b
YatsekK <34369637+YatsekK@users.noreply.github.com>
2018-11-18 23:19:11 +0300

    End of Lesson 2

---
+++ app/src/main/java/com/example/yatsek/moneytracker/AddItemActivity.java
@@ -0,0 +1,58 @@
+package com.example.yatsek.moneytracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    private static final String TAG = "AddItemActivity";
+
+    EditText name;
+    EditText price;
+    Button addBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_additem);
+
+        name = findViewById(R.id.name);
+        price = findViewById(R.id.price);
+        addBtn = findViewById(R.id.add_button);
+
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                addBtn.setEnabled(!TextUtils.isEmpty(s));
+                Log.i(TAG, "afterTextChanged: " + s);
+            }
+        });
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String itemName = name.getText().toString();
+                String itemPrice = price.getText().toString();
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/yatsek/moneytracker/MainActivity.java
+++ app/src/main/java/com/example/yatsek/moneytracker/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.yatsek.moneytracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,36 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity", "onCreate");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("MainActivity", "onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("MainActivity", "onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("MainActivity", "onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("MainActivity", "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("MainActivity", "onDestroy");
     }
 }
