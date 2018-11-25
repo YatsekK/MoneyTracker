630c5482be3f048bee849c9c17c3c79a6c8de989
YatsekK <34369637+YatsekK@users.noreply.github.com>
2018-11-25 19:59:33 +0300

    Activity converted into Fragment. ViewPager added.

---
+++ app/src/main/java/com/example/yatsek/moneytracker/ItemsFragment.java
@@ -0,0 +1,40 @@
+package com.example.yatsek.moneytracker;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ItemsFragment extends Fragment {
+
+    private RecyclerView recycler;
+    private ItemsAdapter adapter;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        adapter = new ItemsAdapter();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_items, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recycler = view.findViewById(R.id.list);
+        recycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        recycler.setAdapter(adapter);
+    }
+}
--- app/src/main/java/com/example/yatsek/moneytracker/MainActivity.java
+++ app/src/main/java/com/example/yatsek/moneytracker/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.yatsek.moneytracker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pager = findViewById(R.id.viewPager);
+        MainPagesAdapter adapter = new MainPagesAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+
     }
 }
